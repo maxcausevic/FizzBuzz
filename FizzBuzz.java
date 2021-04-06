2127e2ee07f72b6bef0a75ee64e3036424f496c8
Maxine Causevic <maxine@fitmaxpersonaltraining.com>
2021-04-06 12:26:19 -0500

    commit

---
+++ FizzBuzz.java
@@ -0,0 +1,21 @@
+public class FizzBuzz {
+
+    public static String fizzBuzz(int number) {
+
+            if(number % 5 == 0 && number % 3 == 0)
+                return "FizzBuzz";
+            if(number%5 ==0)
+                return "Fizz";
+            if(number%3 == 0) 
+                return "Buzz";
+            return Integer.toString(number);
+    }
+    public static void main(String[] args){
+        for (int i=0; i<=100; i++){
+            String result = fizzBuzz(i);
+            String output = String.format("Number: %d: result:%s",i,result);
+            System.out.println(output);
+        }
+    }
+}
+
